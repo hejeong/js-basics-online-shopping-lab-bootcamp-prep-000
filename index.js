ca745bc1bba1c5af7d6a14a4f99032d96694174e
Jonathan Hong <jonhong777@gmail.com>
2018-06-15 17:18:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function viewCart() {
   }
   var outputString = "In your cart, you have "
   for(var i = 0; i < cart.length; i++){
-    if(i === cart.length - 1){
+    if(i > 0 && i === cart.length - 1){
       outputString += `and ${cart[i].itemName} at $${cart[i].itemPrice}.`
     }else{
       outputString += `${cart[i].itemName} at $${cart[i].itemPrice}, `
