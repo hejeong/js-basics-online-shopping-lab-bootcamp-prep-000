0fa189a3dee86ec2c5fe71ed52b660a96b476780
Jonathan Hong <jonhong777@gmail.com>
2018-06-15 17:23:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -41,6 +41,10 @@ function viewCart() {
 
 function total() {
   // write your code here
+  var totalPrice = 0;
+  for(var i = 0; i < cart.length; i++){
+    totalPrice += cart[i].itemPrice
+  }
 }
 
 function removeFromCart(item) {
