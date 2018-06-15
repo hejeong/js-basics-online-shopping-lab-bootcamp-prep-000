932c32e68a9c22a10846df1d07653a119b5f375e
Jonathan Hong <jonhong777@gmail.com>
2018-06-15 17:22:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,12 +27,12 @@ function viewCart() {
   var outputString = "In your cart, you have "
   for(var i = 0; i < cart.length; i++){
     if(cart.length === 1){
-      outputString += `${cart[i].itemName} at $${cart[i].itemName}.`
+      outputString += `${cart[i].itemName} at $${cart[i].itemPrice}.`
     }else{
       if(i === cart.length - 1){
-        outputString += `and ${cart[i].itemName} at $${cart[i].itemName}.`
+        outputString += `and ${cart[i].itemName} at $${cart[i].itemPrice}.`
       }else{
-        outputString += `${cart[i].itemName} at $${cart[i].itemName}, `
+        outputString += `${cart[i].itemName} at $${cart[i].itemPrice}, `
       }
     }
   }
